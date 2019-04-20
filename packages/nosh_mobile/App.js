8658248f17c899a63bebf5665576b42f2a538b9d
Keith Leung <keith@appit.com.hk>
2019-04-20 12:37:50 +0800

    .

--- packages/nosh_mobile/App.js
+++ packages/nosh_mobile/App.js
@@ -8,12 +8,14 @@
 
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Title } from 'nosh_core'
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Nosh Mobile</Text>
+        <Title />
       </View>
     );
   }
